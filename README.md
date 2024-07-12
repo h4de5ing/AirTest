b813ae594171c8e022feb7659d920fe3673b701b
h4de5ing <lex109@qq.com>
2024-07-12 18:16:08 +0800

    update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # 空气质量监测程序
 
-
+[首页](screen/home.png)
+[设置](screen/setting.png)
+[导出](screen/export.png)
--- composeApp/src/commonMain/kotlin/content/ExportContent.kt
+++ composeApp/src/commonMain/kotlin/content/ExportContent.kt
@@ -16,6 +16,31 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 @Composable
 fun ExportContent() {
-
-
+    var startTime by mutableStateOf("")
+    var stopTime by mutableStateOf("")
+    Column(modifier = Modifier.fillMaxSize().padding(10.dp), verticalArrangement = Arrangement.Center) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(text = "起始时间")
+            TextField(value = startTime, onValueChange = { startTime = it }, readOnly = true)
+            Text(text = "结束时间")
+            TextField(value = stopTime, onValueChange = { stopTime = it }, readOnly = true)
+        }
+        Spacer(modifier = Modifier.padding(10.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = {}) {
+                Text(text = "数据导出")
+            }
+            Button(onClick = {}) {
+                Text(text = "数据清除")
+            }
+        }
+    }
 }
